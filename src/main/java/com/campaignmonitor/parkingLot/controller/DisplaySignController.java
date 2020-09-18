c63222767eb211262b4fa9b42af4bd0bf014e8e1
it-tmx <IT@tmxintermodal.com>
2020-09-18 08:05:57 -0400

    first commit

---
+++ src/main/java/com/campaignmonitor/parkingLot/controller/DisplaySignController.java
@@ -0,0 +1,30 @@
+package com.campaignmonitor.parkingLot.controller;
+
+import com.campaignmonitor.parkingLot.model.DisplaySign;
+import com.campaignmonitor.parkingLot.model.FeeRate;
+
+import java.util.List;
+
+/**
+ * DesignSignController
+ *
+ */
+
+public class DisplaySignController {
+
+    public List<DisplaySign> getDisplaySigns() {
+        return null;
+    }
+
+    public DisplaySign getDisplaySignById( Integer id) {
+        return null;
+    }
+
+    public DisplaySign createDisplaySign( DisplaySign displaySign ) {
+        return null;
+    }
+
+    public DisplaySign updateDisplaySign( DisplaySign displaySign ) {
+        return null;
+    }
+}
---
+++ src/main/java/com/campaignmonitor/parkingLot/controller/SpaceController.java
@@ -0,0 +1,30 @@
+package com.campaignmonitor.parkingLot.controller;
+
+import com.campaignmonitor.parkingLot.model.DisplaySign;
+import com.campaignmonitor.parkingLot.model.Space;
+
+import java.util.List;
+
+/**
+ * SpaceController
+ *
+ */
+
+public class SpaceController {
+
+    public List<Space> getSpaces() {
+        return null;
+    }
+
+    public Space getSpaceById( Integer id) {
+        return null;
+    }
+
+    public Space createSpace( Space space ) {
+        return null;
+    }
+
+    public Space updateSpace( Space space ) {
+        return null;
+    }
+}
