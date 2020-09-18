6fe8dc1277d32607b2d8ac591ce841c4437a6393
it-tmx <IT@tmxintermodal.com>
2020-09-18 11:23:42 -0400

    first commit

---
+++ src/main/java/com/campaignmonitor/parkingLot/controller/TicketController.java
@@ -0,0 +1,29 @@
+package com.campaignmonitor.parkingLot.controller;
+
+import com.campaignmonitor.parkingLot.model.Ticket;
+
+import java.util.List;
+
+/**
+ * TicketController
+ *
+ */
+
+public class TicketController {
+
+    public List<Ticket> getTickets() {
+        return null;
+    }
+
+    public Ticket getTicketById( Integer id) {
+        return null;
+    }
+
+    public Ticket createTicket( Ticket space ) {
+        return null;
+    }
+
+    public Ticket updateTicket( Ticket space ) {
+        return null;
+    }
+}
